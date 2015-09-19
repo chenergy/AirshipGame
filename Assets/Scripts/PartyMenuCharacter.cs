767ecd6437e1382edec2c20a0b67c052e1c0c64e
Jonathan Chien <jonathan.chien@gmail.com>
2015-09-18 21:01:29 -0700

    added preliminary party member ui

--- Assets/Scripts/PartyMenuCharacter.cs
+++ Assets/Scripts/PartyMenuCharacter.cs
@@ -5,24 +5,23 @@ using System.Collections;
 
 public class PartyMenuCharacter : MonoBehaviour
 {
+	public Image iconImage;
 	public Text nameText;
 
 	public Image hpImage;
-	public Text hpCurText;
-	public Text hpMaxText;
+	public Text hpText;
 
 	public Image mpImage;
-	public Text mpCurText;
-	public Text mpMaxText;
-
+	public Text mpText;
 	
 
 	public void SetPartyCharacter (Character character){
+		this.iconImage.sprite = character.Icon;
 		this.nameText.text = character.Name;
 		this.hpImage.fillAmount = 1.0f;
 		this.mpImage.fillAmount = 1.0f;
-		this.hpCurText.text = this.hpMaxText.text = character.MaxHp.ToString ();
-		this.mpCurText.text = this.mpMaxText.text = character.MaxMp.ToString ();
+		this.hpText.text = string.Format ("{0} / {1}", "0", character.MaxHp.ToString ());
+		this.mpText.text = string.Format ("{0} / {1}", "0", character.MaxMp.ToString ());
 	}
 }
 
