3d3d1b8ae5f22e020aac37572e6adcd3cec9ee9f
Jonathan Chien <jonathan.chien@gmail.com>
2016-01-30 20:37:15 -0800

    add shadow

---
+++ Assets/Scripts/Model/Airship/AirshipShadow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// The shadow underneath the airship.
+/// </summary>
+public class AirshipShadow : MonoBehaviour
+{
+	/// <summary>
+	/// The airship target that the shadow is following.
+	/// </summary>
+	public A_Airship m_target;
+
+	/// <summary>
+	/// The layer mask of the objects being detected.
+	/// </summary>
+	private int m_layerMask;
+
+	/// <summary>
+	/// Awake this instance.
+	/// </summary>
+	void Awake (){
+		m_layerMask = LayerMask.GetMask (new string[] {"EnvironmentCollider"});
+	}
+
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
+	void Update (){
+		RaycastHit hit;
+		if (m_target != null) {
+			if (Physics.Raycast (m_target.transform.position, Vector3.down, out hit, 100.0f, m_layerMask)) {
+				//this.transform.position = new Vector3 (m_target.transform.position.x, hit.point.y, m_target.transform.position.z);
+				this.transform.position = hit.point;
+			}
+
+			//Vector3 euler = m_target.transform.rotation.eulerAngles;
+			//this.transform.rotation = Quaternion.Euler (new Vector3 (0.0f, euler.y, 0.0f));
+		}
+	}
+}
+
